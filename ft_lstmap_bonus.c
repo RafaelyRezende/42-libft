7076daa770ec1b657fe1c8b05cfc0e81bcd0ff57
Zedro <zedro@zedro.com>
2025-04-16 12:27:53 +0100

    UPDATED ALL

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -15,21 +15,23 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
 {
 	t_list	*res;
-	void	*tmp;
+	t_list	*tmp;
+	void	*cont;
 
 	if (!f || !del)
 		return (NULL);
 	res = NULL;
 	while (lst)
 	{
-		tmp = f(lst->content);
-		res = ft_lstnew(tmp);
+		cont = f(lst->content);
+		tmp = ft_lstnew(cont);
 		if (!tmp)
 		{
-			ft_lstdelone(lst, del(&tmp));
-			break ;
+			del(cont);
+			ft_lstclear(&res, del);
+			return (NULL);
 		}
-		res->next = lst->next;
+		ft_lstadd_back(&res, tmp);
 		lst = lst->next;
 	}
 	return (res);
