1ade4ae025679d26d15107c8f2c1d8ef95189a62
Rafael Rezende <rafael.yanase@gmail.com>
2025-08-11 12:53:03 +0100

    Updated: makefile compile ft_atol.c | ft_atol cast float return value

--- ft_atol.c
+++ ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: rluis-ya <rluis-ya@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 08:54:43 by rluis-ya          #+#    #+#             */
-/*   Updated: 2025/08/11 12:15:36 by rluis-ya         ###   ########.fr       */
+/*   Updated: 2025/08/11 12:27:12 by rluis-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,11 @@ static int	ft_isspace(int c)
 
 long	ft_atoi(const char *nptr)
 {
-	long	sign;
-	long	res;
+	long		sign;
+	long long	res;
 
 	sign = 1;
 	res = 0;
-	if (!ft_strcmp("-2147483648", nptr))
-		return (-2147483648);
 	while (ft_isspace(*nptr))
 		nptr++;
 	while (*nptr == '-' || *nptr == '+')
@@ -46,5 +44,7 @@ long	ft_atoi(const char *nptr)
 		res = res * 10 + *nptr - '0';
 		nptr++;
 	}
-	return (sign * res);
+	if (res < -2147483648 || res > 2147483648) 
+		return (0);
+	return ((long)(sign * res));
 }
