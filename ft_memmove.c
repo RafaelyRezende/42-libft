b9b02c1898ad99030c0e85da1e5d1bcaa3a91b1d
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2025-10-19 18:41:17 +0100

    Chore: norm update.

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rluis-ya <rluis-ya@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 09:15:40 by rluis-ya          #+#    #+#             */
-/*   Updated: 2025/10/19 18:37:05 by jps              ###   ########.fr       */
+/*   Updated: 2025/10/19 18:40:36 by jps              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,13 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    unsigned char	*d = (unsigned char *) dest;
-    const unsigned char	*s = (const unsigned char *) src;
+    unsigned char	*d;
+    const unsigned char	*s;
 
     if (dest == src || n == 0)
 	return (dest);
+    d = (unsigned char *) dest;
+    s = (const unsigned char *) src;
     if (d < s)
 	return (ft_memcpy(dest, src, n));
     d += n;
